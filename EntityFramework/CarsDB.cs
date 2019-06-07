79b4f8a89ce59ca46534e84b732c1d57c47bbd57
davinchi <davinchi>
2019-06-07 21:21:46 +0400

    Entity

---
+++ EntityFramework/CarsDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    class CarsDB : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -10,6 +10,47 @@ namespace EntityFramework
     {
         static void Main(string[] args)
         {
+            using (var db = new CarsDB())
+            {
+                if (!db.Database.Exists())
+                {
+                    InitData();
+                }
+                
+            }
+            QueryData();
+            Console.ReadKey();
         }
+
+        static void QueryData()
+        {
+            CarsDB dbContext = new CarsDB();
+            var cars = dbContext.Cars;
+            dbContext.Database.Log = Console.WriteLine;
+            var query = cars.Select(x => x.name);
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+         
+        static void InitData()
+        {
+            List<Car> cars = new List<Car>();
+            cars.Add(new Car() {name = "BMW", year = 2000 });
+            cars.Add(new Car() {name = "Mersedes", year = 2010 });
+            cars.Add(new Car() {name = "Hondai", year = 2012 });
+            cars.Add(new Car() {name = "Nissan", year = 2006 });
+
+            CarsDB db = new CarsDB();
+                foreach (var item in cars)
+                {
+                    db.Cars.Add(item);
+                }
+
+            db.SaveChanges();
+        } 
+
     }
 }
+
